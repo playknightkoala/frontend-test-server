c8d88931283a09bc0cb919572ddc260ea5d20bcf
yshong <yshong@eland.com.tw>
2024-04-26 15:19:14 +0800

    1. 整理跨域程式碼

---
+++ src/units/cors.js
@@ -0,0 +1,16 @@
+exports.createCors = () => {
+  const cors = require("cors");
+  const whitelist = JSON.parse(process.env.ALLOW_HREF_LIST) || [];
+  return cors((request, callback) => {
+    const origin = request.header("Origin");
+    if (
+      origin &&
+      (whitelist.indexOf(origin) !== -1 || origin.indexOf("localhost") >= 0 || origin.indexOf("127.0.0.1") >= 0)
+    ) {
+      callback(null, true);
+    } else {
+      // eslint-disable-next-line n/no-callback-literal
+      callback("Not allowed by CORS");
+    }
+  });
+};
