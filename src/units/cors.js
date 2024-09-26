f2768e627eae4bff04981b3d0fa945654e09342c
yshong <yshong@eland.com.tw>
2024-09-26 10:27:31 +0800

    調整cors設定

--- src/units/cors.js
+++ src/units/cors.js
@@ -2,7 +2,7 @@ exports.createCors = () => {
   const cors = require("cors");
   const whitelist = JSON.parse(process.env.ALLOW_HREF_LIST) || [];
   return cors((request, callback) => {
-    const origin = request.header("Origin");
+    const origin = request.header("Origin") || request.header("host");
     if (
       origin &&
       (whitelist.indexOf(origin) !== -1 || origin.indexOf("localhost") >= 0 || origin.indexOf("127.0.0.1") >= 0)
